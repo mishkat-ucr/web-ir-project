0ef0debb909812aa0aa02d0a9c2e92c3e09717b8
Shihab Rashid <shihab.rashid12@gmail.com>
2020-02-01 12:43:52 -0800

    final crawler done, test json file added

--- TravelCrawler.py
+++ TravelCrawler.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import praw
 import json
 import datetime as dt
-
-
-# In[2]:
-
+counter = 0
 
 reddit = praw.Reddit(client_id='Jxj7iGi2PGFtRA',
                      client_secret='JJkLXqSWpgyeavjbLKMw-tVk678',
@@ -18,58 +9,41 @@ reddit = praw.Reddit(client_id='Jxj7iGi2PGFtRA',
                      username='Mishkat076',
                      password='Mishkat076')
 
-
-# In[3]:
-
-
 with open('subreddits.txt', 'r') as infile:
     subreddits = infile.readlines()
 
-
-# In[4]:
-
-
 subreddits = [subreddit.rstrip('\n') for subreddit in subreddits]
-
-
-# In[5]:
-
 
 keyval = {
             'title':'title', 
-            'selftext':'body'
+            'selftext':'body',
+            'id':'id',
+            'url':'image',
+            'num_comments':'num_comments',
+            'permalink':'link',
+            'score':'upvotes'
          } # list all key:value except for comments
-
-
-# In[6]:
-
 
 def get_comments(submission):
     submission.comments.replace_more(limit=None)
     return [comment.body for comment in submission.comments.list()]
 
-
-# In[7]:
-
-
 data = []
 for subreddit in subreddits:
-    top = reddit.subreddit(subreddit).top(limit=1)
+    top = reddit.subreddit(subreddit).top(limit=5) #change limits here, max = 999
+    print('collecting subreddit: %s'%subreddit)
     for submission in top:
+        counter+=1
+        print ('post: %d' %counter)
         values = vars(submission)
         datum = {keyval[key]:values[key] for key in keyval.keys()}
         datum['comments'] = get_comments(submission)
         data.append(datum)
 
-
-# In[8]:
-
-
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
 
 
-# In[ ]:
 
 
 
